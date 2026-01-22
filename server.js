835b9e2e8ba19a62d69be955a5e9d1fe94b686ed
KaeTheDev <kaethedev@gmail.com>
2026-01-21 23:24:53 -0500

    Created server code and updated db connection file

---
+++ server.js
@@ -0,0 +1,29 @@
+// Import Libraries
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db'); // DB Connection
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Create Express App
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+
+// Test route to confirm server is running
+app.get('/', (req, res) => {
+    res.send('Server is running! 🚀');
+});
+
+// Mount User and Bookmark routes
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`);
+});
